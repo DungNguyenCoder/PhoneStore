bf29cf958d635b82e321ecf1dba7e3f0405b4912
DungNguyenCoder <nm.dung.it@gmail.com>
2026-02-19 12:18:18 +0700

    Update

--- SellPhoneWebsite/Controllers/HomeController.cs
+++ SellPhoneWebsite/Controllers/HomeController.cs
@@ -2,16 +2,36 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SellPhoneWebsite.Data;
 using SellPhoneWebsite.Models;
 
 namespace SellPhoneWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
-        public IActionResult Index()
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var res = new ProductHomePage();
+
+            res.LatestproductList = await _context.Product.Include(p => p.Category).Include(p => p.ProductImages)
+                .OrderByDescending(p => p.CreateAt)
+                .Take(8)
+                .ToListAsync();
+
+            res.BestSaleProduct = await _context.Product.Include(p => p.Category).Include(p => p.ProductImages).Include(p => p.OrderItems)
+                .OrderByDescending(p => p.OrderItems.Count())
+                .Take(8)
+                .ToListAsync();
+
+
+            return View(res);
         }
 
         public async Task<IActionResult> Privacy()
---
+++ SellPhoneWebsite/Models/ProductHomePage.cs
@@ -0,0 +1,12 @@
+﻿using SellPhoneWebsite.Models.Entities;
+
+namespace SellPhoneWebsite.Models
+{
+    public class ProductHomePage
+    {
+        // chỉ lấy 8 sản phẩm mới nhất
+        public IEnumerable<Product>? LatestproductList { get; set; }
+        // chỉ lấy 8 sản phẩm bán chạy nhất
+        public IEnumerable<Product>? BestSaleProduct { get; set; }
+    }
+}
